49a0e52385d949262cae4ad8ea703540eae47ae4
Leonard Carcaramo <lcarcaramo@ibm.com>
2022-10-25 14:03:57 -0400

    Debug segment builder.
    Signed-off-by: Leonard Carcaramo <lcarcaramo@ibm.com>

--- pyzkiln_core/pyracf/R_admin/C/uadmin/uadmin_kv.c
+++ pyzkiln_core/pyracf/R_admin/C/uadmin/uadmin_kv.c
@@ -31,7 +31,7 @@ int count_base_segment_fields(BASE_SEGMENT_T *);
 RC uadmin_build_omvs_segment(BYTE *, BASE_SEGMENT_T *, LOGGER_T *);
 int count_omvs_segment_fields(OMVS_SEGMENT_T *);
 void build_segment_header(BYTE, const char *, int);
-RC add_key_value_field(BYTE *, char *, char *, KV_T *, LOGGER_T *);
+RC add_key_value_field(BYTE *, char *, const char *, KV_T *, LOGGER_T *);
 void add_boolean_field(BYTE *, char *);
 RC convert_to_ebcdic(char *, char *, char [], int, LOGGER_T *);
 KV_CTL_T *uadmin_kv_init(LOGGER_T *);
@@ -196,7 +196,7 @@ void build_segment_header(BYTE *finger, const char *ebcdic_key, int field_count)
    finger += sizeof(int);
 }
 
-RC add_key_value_field(BYTE *finger, char *eye_catcher, char *ebcdic_key, KV_T *pKV, LOGGER_T *pLog) {
+RC add_key_value_field(BYTE *finger, char *eye_catcher, const char *ebcdic_key, KV_T *pKV, LOGGER_T *pLog) {
    RC rc = SUCCESS;
    // Set key
    int l_ebcdic_key = sizeof(ebcdic_key);
