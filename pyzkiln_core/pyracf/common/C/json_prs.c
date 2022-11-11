fa9c8cabfba7e218629028b0f1c3c89e12ae685d
Leonard Carcaramo <lcarcaramo@ibm.com>
2022-11-11 08:10:45 -0500

    Debug booleans.
    Signed-off-by: Leonard Carcaramo <lcarcaramo@ibm.com>

--- pyzkiln_core/pyracf/common/C/json_prs.c
+++ pyzkiln_core/pyracf/common/C/json_prs.c
@@ -139,6 +139,9 @@ int value(JSKV_CTL_T *pJKCtl)
          printf("I'm going to try to add the value.\n");
          printf("%d %d\n", (ICH(index) >= 5), (strncmp(PCH(index), "false", 5)));
          printf("%d %d\n", (ICH(index) >= 5), (strncmp(PCH(pinky), "false", 5)));
+         char string[5];
+         memcpy(string, PCH(index), 5);
+         printf("%s\n", string);
          if ((ICH(index) >= 5) && (strncmp(PCH(index), "false", 5)))
             {
              printf("I am adding the value.");
