20c57e3062fe514d4f7b73536a34aab615531f8b
Leonard Carcaramo <lcarcaramo@ibm.com>
2022-11-16 12:53:48 -0500

    Debug structures and population of 31 bit area.
    Signed-off-by: Leonard Carcaramo <lcarcaramo@ibm.com>

--- pyzkiln_core/pyracf/R_admin/C/uadmin/uadmin_dump.c
+++ pyzkiln_core/pyracf/R_admin/C/uadmin/uadmin_dump.c
@@ -123,7 +123,7 @@ void* uadmin_print_fields(BYTE* finger, int nFields, BYTE *pParms, LOGGER_T *pLo
           // Create temporary buffer that is the size of field data plus one to make it null terminated.
           field_data_tmp = calloc(p_fld->l_data + 1, sizeof(char));
           // copy field_data to field_data_tmp as ASCII.
-          tc_e2a(field_data, &(field_data_tmp[0]), p_fld->l_data, pLog);
+          tc_e2a(&(field_data[0]), &(field_data_tmp[0]), p_fld->l_data, pLog);
           printf("  data: %s", field_data_tmp);
           free(field_data_tmp);
        }
