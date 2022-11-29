0cbd13e19479c56081218b06785fc41608f99422
Leonard Carcaramo <lcarcaramo@ibm.com>
2022-11-29 11:53:51 -0500

    Debug uadmin_print
    Signed-off-by: Leonard Carcaramo <lcarcaramo@ibm.com>

--- pyzkiln_core/pyracf/R_admin/C/uadmin/uadmin_dump.c
+++ pyzkiln_core/pyracf/R_admin/C/uadmin/uadmin_dump.c
@@ -138,11 +138,11 @@ void* uadmin_print_fields(BYTE* finger, int nFields, BYTE *pParms, LOGGER_T *pLo
           if (field_data_tmp == NULL) 
              return NULL;
           printf("POINTERS: field_data: %p    |     field_data_tmp:%p    |\n", field_data, field_data_tmp);
-          printf("CONTENTS: field_data: %16s    |     field_data_tmp:%16s    |\n", &field_data, &field_data_tmp);
-          printf("first byte of field_data: %2x", field_data[0]);
-          printf("first byte of field_data_tmp: %2x", field_data_tmp[0]);
-          printf("last byte of field_data: %2x", field_data[p_fld->l_data]);
-          printf("last byte of field_data_tmp: %2x", field_data_tmp[p_fld->l_data]);
+          printf("CONTENTS: field_data: %16s    |     field_data_tmp:%16s    |\n", field_data, field_data_tmp);
+          printf("first byte of field_data: %d\n", field_data[0]);
+          printf("first byte of field_data_tmp: %d\n", field_data_tmp[0]);
+          printf("last byte of field_data: %d\n", field_data[p_fld->l_data]);
+          printf("last byte of field_data_tmp: %d\n", field_data_tmp[p_fld->l_data]);
           // copy field_data to field_data_tmp as ASCII.
           tc_e2a(field_data, field_data_tmp, p_fld->l_data, pLog);
           printf("  data: %s\n", field_data_tmp);
