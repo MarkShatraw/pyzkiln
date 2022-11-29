01d1f2a4491df6e3efc2aba6653255493bcad7a4
Leonard Carcaramo <lcarcaramo@ibm.com>
2022-11-29 12:07:07 -0500

    Update Python and ZOAU configuration.
    Signed-off-by: Leonard Carcaramo <lcarcaramo@ibm.com>

--- pyzkiln_core/pyracf/R_admin/C/uadmin/uadmin_dump.c
+++ pyzkiln_core/pyracf/R_admin/C/uadmin/uadmin_dump.c
@@ -138,12 +138,6 @@ void* uadmin_print_fields(BYTE* finger, int nFields, BYTE *pParms, LOGGER_T *pLo
           field_data_tmp = (char *)calloc(p_fld->l_data + 1, sizeof(char));
           if (field_data_tmp == NULL) 
              return NULL;
-          printf("POINTERS: field_data: %p    |     field_data_tmp:%p    |\n", field_data, field_data_tmp);
-          printf("CONTENTS: field_data: %9s    |     field_data_tmp:%9s    |\n", field_data, field_data_tmp);
-          printf("first byte of field_data: %d\n", field_data[0]);
-          printf("first byte of field_data_tmp: %d\n", field_data_tmp[0]);
-          printf("last byte of field_data: %d\n", field_data[p_fld->l_data]);
-          printf("last byte of field_data_tmp: %d\n", field_data_tmp[p_fld->l_data]);
           // copy field_data to field_data_tmp as ASCII.
           tc_e2a(field_data, field_data_tmp, p_fld->l_data, pLog);
           printf("  data: %s\n", field_data_tmp);
