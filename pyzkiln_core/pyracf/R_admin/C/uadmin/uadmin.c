bba6c602d0b8f14ef5cf09c3407bbfccc97bfe8d
Leonard Carcaramo <lcarcaramo@ibm.com>
2022-11-30 12:21:53 -0500

    Debug uadmin
    Signed-off-by: Leonard Carcaramo <lcarcaramo@ibm.com>

--- pyzkiln_core/pyracf/R_admin/C/uadmin/uadmin.c
+++ pyzkiln_core/pyracf/R_admin/C/uadmin/uadmin.c
@@ -212,7 +212,7 @@ RC build_31bit_args(UADMIN_CTL_T *pUADMINCtl, R_ADMIN_CTL_T *pRACtl)
         // argument list.
         *((unsigned int *__ptr32)&p31->arg_list.ppOutbuf) |= 0x80000000;
 
-        uadmin_p31_raw_dump(p31);
+        uadmin_p31_raw_dump(p31->arg_list);
 
         //uadmin_dump_args_parms(pUADMINCtl, pUADMINCtl->pLog);
        }
